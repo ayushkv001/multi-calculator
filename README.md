3e10ddc931d1113ef1b53c642d17a95522747133
Ayush Kumar <ayushmrityunjay@gmail.com>
2022-07-12 18:16:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A calculator with three modes
+1.Simple(Standard)
+2.Scientific
+3.Programmer (Under Development)
---
+++ js-support-file/calc-type.js
@@ -0,0 +1,15 @@
+function my2(){
+    document.getElementById("b1").style.display='none';
+    document.getElementById("b3").style.display='none';
+    document.getElementById("b2").style.display='block';
+};
+function my1(){
+    document.getElementById("b3").style.display='none';
+    document.getElementById("b2").style.display='none';
+    document.getElementById("b1").style.display='block';
+};
+function my3(){
+    document.getElementById("b1").style.display='none';
+    document.getElementById("b2").style.display='none';
+    document.getElementById("b3").style.display='block';   
+}
